422bec01a642fed02340a37fba5abe122f6b2fc1
Harol johany Zuluaga López <harolz87@gmail.com>
2020-05-30 15:25:51 -0500

    [front] add no found page

---
+++ front/src/pages/NoFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoFound = () => (
+  <div>
+    404
+  </div>
+);
+
+export default NoFound;
--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { PageContainer } from 'Components/PageContainer';
+import { PageContainer } from 'Containers/PageContainer';
 
 import PrivateRoute from './PrivateRoute';
 
+const NoFound = React.lazy(() => import('../pages/NoFound'));
 const Login = React.lazy(() => import('../pages/Login'));
 const Home = React.lazy(() => import('../pages/Home'));
 const Apps = React.lazy(() => import('../pages/Applications'));
 const CheckInOut = React.lazy((() => import('../pages/CheckInOut')));
 
 export const Routes = () => (
-  <PageContainer>
-    <BrowserRouter>
+  <BrowserRouter>
+    <PageContainer>
       <Switch>
         <PrivateRoute exact path="/" from component={Home} />
         <Route exact path="/login" from component={Login} />
         <PrivateRoute exact path="/apps" from component={Apps} />
         <PrivateRoute exact path="/check-in-out" from component={CheckInOut} />
+        <Route component={NoFound} />
       </Switch>
-    </BrowserRouter>
-  </PageContainer>
+    </PageContainer>
+  </BrowserRouter>
 );
