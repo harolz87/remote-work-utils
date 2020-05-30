8a2dcf1565a7e8bfaefb2f5cd329127e863e6f65
Harol johany Zuluaga López <harolz87@gmail.com>
2020-05-30 09:42:42 -0500

    [front] config webpack for react

---
+++ front/webpack/webpack.dev.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: [
+      path.resolve(__dirname, '../src/index.js'),
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name].js',
+    chunkFilename: 'js/chunk_[id].js',
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|gif|svg|webp)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1000000,
+            fallback: 'file-loader',
+            name: 'images/[name].[hash].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
