d6d4ca6560c7becc95c72ee61945fdb1c461be59
Harol johany Zuluaga López <harolz87@gmail.com>
2020-05-30 17:35:08 -0500

    add header withRouter

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { withRouter } from 'react-router-dom';
 
-export const Header = () => (
+export const Header = withRouter(({ history }) => (
   <Navbar bg="primary" expand="lg">
-    <Navbar.Brand href="/home" className="text-white font-weight-bold">RWU</Navbar.Brand>
+    <Navbar.Brand onClick={() => history.push('./')} className="text-white font-weight-bold">RWU</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="/apps" className="text-white">Apps</Nav.Link>
-        <Nav.Link href="/check-in-out" className="text-white">Check In/Out</Nav.Link>
+        <Nav.Link onClick={() => history.push('./apps')} className="text-white">Apps</Nav.Link>
+        <Nav.Link onClick={() => history.push('./check-in-out')} className="text-white">Check In/Out</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
-);
+));
