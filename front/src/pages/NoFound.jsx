dbb7d8ff3d87651ee4726751da52a5530909f99a
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-05-31 13:27:11 -0500

    404

--- front/src/pages/NoFound.jsx
+++ front/src/pages/NoFound.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const NoFound = () => (
-  <div>
-    404
+  <div style={{display:"flex", justifyContent:'center', alignItems:'center', width:'100%', height:'100%', fontSize:'60px'}} >
+ <div style={{width:'50%', textAlign:'center'}}>  404
+   Lo sentimos no encontramos lo que estás buscando :(
+    </div>
   </div>
 );
 
