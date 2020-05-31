d7e13df47b4a864cff0bfb3a304e214a594673d2
juanest12 <juanest12@gmail.com>
2020-05-31 01:36:01 -0500

    [back] Turn off email validation

--- back/rwu/users/serializers/users.py
+++ back/rwu/users/serializers/users.py
@@ -96,8 +96,8 @@ class UserLoginSerializer(serializers.Serializer):
         user = authenticate(username=data['email'], password=data['password'])
         if not user:
             raise serializers.ValidationError('Invalid credentials')
-        if not user.is_verified:
-            raise serializers.ValidationError('Account is not active yet :(')
+        # if not user.is_verified:
+        #     raise serializers.ValidationError('Account is not active yet :(')
         self.context['user'] = user
         return data
 
